8446e334d5adb955026112e76afb30a844d0a24e
FranciscoCharles <charlesdcf123@gmail.com>
2021-06-05 18:14:51 -0300

    rename file

---
+++ src/caesars_cipher.js
@@ -0,0 +1,25 @@
+const space = ' '
+const numbers_set = '0123456789'
+const char_set = 'abcdefghijklmnopqrstuvwxyz'
+
+export const keys = (char_set + char_set.toLocaleUpperCase()
+	+ space + numbers_set).split('')
+
+export const total_keys = keys.length
+
+export function decode_letter(letter, shift) {
+	if (letter === '\n') {
+		return letter
+	}
+	let rest = (letter - shift) % total_keys
+	return (rest > 0) ? rest : total_keys - rest
+}
+export function encode_letter(letter, shift) {
+	return (letter !== '\n') ? (keys.indexOf(letter) + shift) % total_keys : letter
+}
+
+export const caesars_cipher = (text, shift) => {
+	return text.split('').reduce((acc, letter) => {
+		return acc + ((letter !== '\n') ? keys[encode_letter(letter, shift)] : letter)
+	}, '')
+}
