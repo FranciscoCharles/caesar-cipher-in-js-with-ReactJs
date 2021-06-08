ce0021b505892e6d6a64e224c6168d696ba45445
FranciscoCharles <charlesdcf123@gmail.com>
2021-06-08 15:30:31 -0300

    remove inutil paths

--- src/components/input_number.jsx
+++ src/components/input_number.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import styled from 'styled-components'
 import PropTypes from 'prop-types'
 
-import { getInputValue } from '../../util'
+import { getInputValue } from '../util'
 
 const InputNumberStyled = styled.div`
 
